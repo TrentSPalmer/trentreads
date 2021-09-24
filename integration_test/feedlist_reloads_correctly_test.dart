215e5d5fb9f88e1f3d5c37412f1e0a4603e48875
Trent Palmer <trenttdually@gmail.com>
2021-09-24 08:29:04 -0700

    fix integration_test/feedlist_reloads_correctly_test.dart

--- integration_test/feedlist_reloads_correctly_test.dart
+++ integration_test/feedlist_reloads_correctly_test.dart
@@ -5,12 +5,10 @@ import '../lib/home/feeds.dart';
 import '../lib/database/database_helper.dart';
 import '../lib/main.dart';
 
-
 void feedListReloadsCorrectly(String _testDesc) =>
     testWidgets(_testDesc, (tester) async {
       await tester.pumpWidget(MyApp());
-      final FeedState myFeedState =
-          tester.state(find.byType(FeedWidget));
+      final FeedState myFeedState = tester.state(find.byType(FeedWidget));
       expect(myFeedState.feedList.length == 0, true);
 
       int i = 0;
@@ -23,8 +21,14 @@ void feedListReloadsCorrectly(String _testDesc) =>
       String _deletedFeedTitle = await dbHelper.deleteFeed(1);
       await myFeedState.reloadFeeds();
       await tester.pumpAndSettle();
-      expect((myFeedState.feedList.length == (_currentFeedListLength - 1)),
-          true);
+      int j = 0;
+      while ((myFeedState.feedList.length != (_currentFeedListLength - 1)) &&
+          (j < 200)) {
+        await tester.pumpAndSettle();
+        j++;
+      }
+      expect(
+          (myFeedState.feedList.length == (_currentFeedListLength - 1)), true);
       List<String> _listTitles = [];
       myFeedState.feedList.forEach((ScrollableFeed x) {
         _listTitles.add(x.title);
