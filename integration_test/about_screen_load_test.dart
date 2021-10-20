77e9cd81d5d19e116d9daa9c515a7a9c04126ff2
Trent Palmer <trenttdually@gmail.com>
2021-10-19 22:29:02 -0700

    extent integration_test/about_screen_load_test.dart

--- integration_test/about_screen_load_test.dart
+++ integration_test/about_screen_load_test.dart
@@ -1,8 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_test/flutter_test.dart';
+import '../lib/about/about_html.dart';
 
 import '../lib/home/feeds.dart';
 import '../lib/main.dart';
+import 'about_feed_load_test.dart';
 import 'setting_screen_loadtest.dart';
+import 'storage_setting_loadtest.dart';
 
 void aboutScreenLoadTest(String _testDesc) =>
     testWidgets(_testDesc, (tester) async {
@@ -15,4 +20,57 @@ void aboutScreenLoadTest(String _testDesc) =>
         i++;
       }
       await goToSettingsPage(tester);
+      await goToAboutSettingsPage(tester);
+      await verifyAppBarTitle(tester, "About TrentReads");
+      await verifyHomeButton(tester);
+      await goToSettingsPage(tester);
+      await goToAboutSettingsPage(tester);
+      await verifyOKButton(tester, "Settings");
+      await goToAboutSettingsPage(tester);
+      await goToAboutTrentReadsPage(tester);
+      await verifyAppBarTitle(tester, "About TrentReads");
+      await findSingleChildScrollViewAndHtml(tester);
+
+      await clickBackButton(tester);
     });
+
+Future<void> findShowLicenseTile(WidgetTester tester) async {}
+
+Future<void> findSingleChildScrollViewAndHtml(WidgetTester tester) async {
+  Finder singleChildScrollViewFinder = find.byType(SingleChildScrollView);
+  expect(singleChildScrollViewFinder, findsOneWidget);
+  Finder htmlFinder = find.byType(Html);
+  expect(htmlFinder, findsOneWidget);
+  Html myAboutHtml = htmlFinder.evaluate().single.widget as Html;
+  expect(myAboutHtml.data == aboutTrentReads.data, true);
+}
+
+Future<void> goToAboutTrentReadsPage(WidgetTester tester) async {
+  Finder aboutNavTileFinder =
+      find.byKey(Key('nav_tile_for_${settingNavTilesStringList[3]}'));
+  expect(aboutNavTileFinder, findsOneWidget);
+  Row aboutNavTile = aboutNavTileFinder.evaluate().single.widget as Row;
+  Finder inkWellFinder = find.descendant(
+    of: find.byWidget(aboutNavTile),
+    matching: find.byType(InkWell),
+  );
+  expect(inkWellFinder, findsOneWidget);
+  InkWell navTileInkWell = inkWellFinder.evaluate().single.widget as InkWell;
+  await tester.tap(find.byWidget(navTileInkWell));
+  await tester.pumpAndSettle();
+}
+
+Future<void> goToAboutSettingsPage(WidgetTester tester) async {
+  Finder aboutNavTileFinder =
+      find.byKey(Key('nav_tile_for_${settingNavTilesStringList[3]}'));
+  expect(aboutNavTileFinder, findsOneWidget);
+  Row aboutNavTile = aboutNavTileFinder.evaluate().single.widget as Row;
+  Finder inkWellFinder = find.descendant(
+    of: find.byWidget(aboutNavTile),
+    matching: find.byType(InkWell),
+  );
+  expect(inkWellFinder, findsOneWidget);
+  InkWell navTileInkWell = inkWellFinder.evaluate().single.widget as InkWell;
+  await tester.tap(find.byWidget(navTileInkWell));
+  await tester.pumpAndSettle();
+}
